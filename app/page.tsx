4f9fdeca6089880d84ff719c277c4e21fff966d4
Dennis Lau <31469650+dzlau@users.noreply.github.com>
2026-02-05 09:04:50 -0800

    Correct isEven function implementation
    Fix isEven function syntax and logic.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-
+const isEven = (number:n) =>{
+if (n/2) == 0{
+return true
+}
+return false
+}
 export default function Home() {
   return (
     <div className=" font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
